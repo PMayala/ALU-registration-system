4c0acf2d9816a85665bba93979fd554f19bfb679
jumulisa <j.umulisa@gmail.com>
2023-10-14 04:00:29 -0700

    move_to_diectory.sh file was created & and negpod_8-q1 directory was created

---
+++ move-to-directory.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+# Create the target directory if it doesn't exist
+mkdir -p negpod_8-q1
+
+# Move the files to the target directory
+mv main.sh Students-list_0923.txt Select-emails.sh student-emails.txt negpod_8-q1
+
+echo "Files moved to negpod_8-q1 directory."
