9a30b1bb6ed530550eecd4f92104a58da0e5f57d
KAMANZI-SERGE187 <sergekmz187@gmail.com>
2023-10-16 13:13:11 -0700

    function

---
+++ negpod_8-q1/function.sh
@@ -0,0 +1,8 @@
+ # Function to update a student record by Student ID
+   update_student_by_id() {
+        echo "Enter Student ID to update:"
+        read student_id
+        echo "Enter new Student Email:"
+        read new_email
+        echo "Enter new Age:"
+        read new_age
